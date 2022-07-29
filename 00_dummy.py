ac9a9a70be4627fc0736a89bdf2cdf23e636e646
Oleksii <shotokan@ukr.net>
2022-07-29 10:15:34 +0200

    Update and rename counter_prom.py to 00_dummy.py

---
+++ 00_dummy.py
@@ -0,0 +1,20 @@
+import http.server
+import random
+from prometheus_client import start_http_server
+
+APP_PORT = 8000
+METRICS_PORT = 8001
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):        
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Hello_IS</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Hello IS</center></h2></body></html>", "utf-8"))
+        self.wfile.close()
+
+if __name__ == "__main__":
+    start_http_server(METRICS_PORT)
+    server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    server.serve_forever()
